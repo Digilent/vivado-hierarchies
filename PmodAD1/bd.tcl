b4e486f40c5e15210e4cf6d09f2db685c79589e5
ArtVVB <arthur.brown@digilent.com>
2019-07-25 12:27:32 -0700

    use write_bd_tcl output scripts

---
+++ PmodAD1/bd.tcl
@@ -0,0 +1,211 @@
+
+################################################################
+# This is a generated script based on design: design_1
+#
+# Though there are limitations about the generated script,
+# the main purpose of this utility is to make learning
+# IP Integrator Tcl commands easier.
+################################################################
+
+namespace eval _tcl {
+proc get_script_folder {} {
+   set script_path [file normalize [info script]]
+   set script_folder [file dirname $script_path]
+   return $script_folder
+}
+}
+variable script_folder
+set script_folder [_tcl::get_script_folder]
+
+################################################################
+# Check if script is running in correct Vivado version.
+################################################################
+set scripts_vivado_version 2019.1
+set current_vivado_version [version -short]
+
+if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
+   puts ""
+   common::send_msg_id "BD_TCL-1002" "WARNING" "This script was generated using Vivado <$scripts_vivado_version> without IP versions in the create_bd_cell commands, but is now being run in <$current_vivado_version> of Vivado. There may have been major IP version changes between Vivado <$scripts_vivado_version> and <$current_vivado_version>, which could impact the parameter settings of the IPs."
+
+}
+
+################################################################
+# START
+################################################################
+
+# To test this script, run the following commands from Vivado Tcl console:
+# source design_1_script.tcl
+
+
+# The design that will be created by this Tcl script contains the following 
+# module references:
+# PmodAD1_AXI, PmodAD1_remap
+
+# Please add the sources of those modules before sourcing this Tcl script.
+
+set bCheckIPsPassed 1
+##################################################################
+# CHECK IPs
+##################################################################
+set bCheckIPs 1
+if { $bCheckIPs == 1 } {
+   set list_check_ips "\ 
+digilentinc.com:ip:pmod_bridge:*\
+"
+
+   set list_ips_missing ""
+   common::send_msg_id "BD_TCL-006" "INFO" "Checking if the following IPs exist in the project's IP catalog: $list_check_ips ."
+
+   foreach ip_vlnv $list_check_ips {
+      set ip_obj [get_ipdefs -all $ip_vlnv]
+      if { $ip_obj eq "" } {
+         lappend list_ips_missing $ip_vlnv
+      }
+   }
+
+   if { $list_ips_missing ne "" } {
+      catch {common::send_msg_id "BD_TCL-115" "ERROR" "The following IPs are not found in the IP Catalog:\n  $list_ips_missing\n\nResolution: Please add the repository containing the IP(s) to the project." }
+      set bCheckIPsPassed 0
+   }
+
+}
+
+##################################################################
+# CHECK Modules
+##################################################################
+set bCheckModules 1
+if { $bCheckModules == 1 } {
+   set list_check_mods "\ 
+PmodAD1_AXI\
+PmodAD1_remap\
+"
+
+   set list_mods_missing ""
+   common::send_msg_id "BD_TCL-006" "INFO" "Checking if the following modules exist in the project's sources: $list_check_mods ."
+
+   foreach mod_vlnv $list_check_mods {
+      if { [can_resolve_reference $mod_vlnv] == 0 } {
+         lappend list_mods_missing $mod_vlnv
+      }
+   }
+
+   if { $list_mods_missing ne "" } {
+      catch {common::send_msg_id "BD_TCL-115" "ERROR" "The following module(s) are not found in the project: $list_mods_missing" }
+      common::send_msg_id "BD_TCL-008" "INFO" "Please add source files for the missing module(s) above."
+      set bCheckIPsPassed 0
+   }
+}
+
+if { $bCheckIPsPassed != 1 } {
+  common::send_msg_id "BD_TCL-1003" "WARNING" "Will not continue with creation of design due to the error(s) above."
+  return 3
+}
+
+##################################################################
+# DESIGN PROCs
+##################################################################
+
+
+# Hierarchical cell: PmodAD1_0
+proc create_hier_cell_PmodAD1_0 { parentCell nameHier } {
+
+  variable script_folder
+
+  if { $parentCell eq "" || $nameHier eq "" } {
+     catch {common::send_msg_id "BD_TCL-102" "ERROR" "create_hier_cell_PmodAD1_0() - Empty argument(s)!"}
+     return
+  }
+
+  # Get object for parentCell
+  set parentObj [get_bd_cells $parentCell]
+  if { $parentObj == "" } {
+     catch {common::send_msg_id "BD_TCL-100" "ERROR" "Unable to find parent cell <$parentCell>!"}
+     return
+  }
+
+  # Make sure parentObj is hier blk
+  set parentType [get_property TYPE $parentObj]
+  if { $parentType ne "hier" } {
+     catch {common::send_msg_id "BD_TCL-101" "ERROR" "Parent <$parentObj> has TYPE = <$parentType>. Expected to be <hier>."}
+     return
+  }
+
+  # Save current instance; Restore later
+  set oldCurInst [current_bd_instance .]
+
+  # Set parent object as current
+  current_bd_instance $parentObj
+
+  # Create cell and set as current instance
+  set hier_obj [create_bd_cell -type hier $nameHier]
+  current_bd_instance $hier_obj
+
+  # Create interface pins
+  create_bd_intf_pin -mode Slave -vlnv xilinx.com:interface:aximm_rtl:1.0 AXI_LITE_SAMPLE
+
+  create_bd_intf_pin -mode Master -vlnv digilentinc.com:interface:pmod_rtl:1.0 Pmod_out
+
+
+  # Create pins
+  create_bd_pin -dir I -type clk s_axi_aclk
+  create_bd_pin -dir I -type rst s_axi_aresetn
+
+  # Create instance: PmodAD1_AXI_0, and set properties
+  set block_name PmodAD1_AXI
+  set block_cell_name PmodAD1_AXI_0
+  if { [catch {set PmodAD1_AXI_0 [create_bd_cell -type module -reference $block_name $block_cell_name] } errmsg] } {
+     catch {common::send_msg_id "BD_TCL-105" "ERROR" "Unable to add referenced block <$block_name>. Please add the files for ${block_name}'s definition into the project."}
+     return 1
+   } elseif { $PmodAD1_AXI_0 eq "" } {
+     catch {common::send_msg_id "BD_TCL-106" "ERROR" "Unable to referenced block <$block_name>. Please add the files for ${block_name}'s definition into the project."}
+     return 1
+   }
+  
+  # Create instance: PmodAD1_remap_0, and set properties
+  set block_name PmodAD1_remap
+  set block_cell_name PmodAD1_remap_0
+  if { [catch {set PmodAD1_remap_0 [create_bd_cell -type module -reference $block_name $block_cell_name] } errmsg] } {
+     catch {common::send_msg_id "BD_TCL-105" "ERROR" "Unable to add referenced block <$block_name>. Please add the files for ${block_name}'s definition into the project."}
+     return 1
+   } elseif { $PmodAD1_remap_0 eq "" } {
+     catch {common::send_msg_id "BD_TCL-106" "ERROR" "Unable to referenced block <$block_name>. Please add the files for ${block_name}'s definition into the project."}
+     return 1
+   }
+  
+  # Create instance: pmod_bridge_0, and set properties
+  set pmod_bridge_0 [ create_bd_cell -type ip -vlnv digilentinc.com:ip:pmod_bridge pmod_bridge_0 ]
+  set_property -dict [ list \
+   CONFIG.Bottom_Row_Interface {Disabled} \
+   CONFIG.PMOD {Custom} \
+   CONFIG.Top_Row_Interface {GPIO} \
+   CONFIG.USE_BOARD_FLOW {true} \
+ ] $pmod_bridge_0
+
+  # Create interface connections
+  connect_bd_intf_net -intf_net AXI_LITE_SAMPLE_1 [get_bd_intf_pins AXI_LITE_SAMPLE] [get_bd_intf_pins PmodAD1_AXI_0/S_AXI]
+  connect_bd_intf_net -intf_net Conn1 [get_bd_intf_pins Pmod_out] [get_bd_intf_pins pmod_bridge_0/Pmod_out]
+  connect_bd_intf_net -intf_net PmodAD1_remap_0_GPIO_TOP [get_bd_intf_pins PmodAD1_remap_0/GPIO_TOP] [get_bd_intf_pins pmod_bridge_0/GPIO_Top_Row]
+
+  # Create port connections
+  connect_bd_net -net PmodAD1_AXI_0_ad1_cs [get_bd_pins PmodAD1_AXI_0/ad1_cs] [get_bd_pins PmodAD1_remap_0/ad1_cs]
+  connect_bd_net -net PmodAD1_AXI_0_ad1_sclk [get_bd_pins PmodAD1_AXI_0/ad1_sclk] [get_bd_pins PmodAD1_remap_0/ad1_sclk]
+  connect_bd_net -net PmodAD1_remap_0_ad1_sdin0 [get_bd_pins PmodAD1_AXI_0/ad1_sdin0] [get_bd_pins PmodAD1_remap_0/ad1_sdin0]
+  connect_bd_net -net PmodAD1_remap_0_ad1_sdin1 [get_bd_pins PmodAD1_AXI_0/ad1_sdin1] [get_bd_pins PmodAD1_remap_0/ad1_sdin1]
+  connect_bd_net -net s_axi_aclk_1 [get_bd_pins s_axi_aclk] [get_bd_pins PmodAD1_AXI_0/S_AXI_ACLK]
+  connect_bd_net -net s_axi_aresetn_1 [get_bd_pins s_axi_aresetn] [get_bd_pins PmodAD1_AXI_0/S_AXI_ARESETN]
+
+  # Restore current instance
+  current_bd_instance $oldCurInst
+}
+
+
+proc available_tcl_procs { } {
+   puts "##################################################################"
+   puts "# Available Tcl procedures to recreate hierarchical blocks:"
+   puts "#"
+   puts "#    create_hier_cell_PmodAD1_0 parentCell nameHier"
+   puts "#"
+   puts "##################################################################"
+}
+
+available_tcl_procs
---
+++ PmodAD1/create_hier.tcl
@@ -0,0 +1,41 @@
+# This script manages dependencies and name resolution for the bd.tcl script
+
+# Determine if any IP dependencies are missing, if they are, add the repo directory to the repo search paths.
+# Care should be taken to avoid duplicates...
+proc ip_not_found {} {
+	# Add any IP dependencies included in the repo directory here
+	set ip_vlnvs [list \
+		digilentinc.com:ip:pmod_bridge:* \
+	]
+	foreach vlnv $ip_vlnvs {
+		if { [llength [get_ipdefs $vlnv]] == 0 } {
+			return 1
+		}
+	}
+	return 0
+}
+if { [ip_not_found] } {
+	set ip_repo_list [get_property IP_REPO_PATHS [current_project]]
+	lappend ip_repo_list [file join [file dirname [file dirname [info script]]] repo]
+	set_property IP_REPO_PATHS $ip_repo_list [current_project]
+	update_ip_catalog
+}
+
+# Add source files to the project
+set list_source_files [glob -nocomplain [file join [file dirname [info script]] sources *]]
+if { [llength $list_source_files] > 0 } {
+	# adding the same file more than once is okay
+	catch { add_files -fileset sources_1 $list_source_files }
+}
+
+#set list_constr_files [glob -nocomplain [file join [file dirname [info script]] constrs *]]
+#catch { import_files -fileset constrs_1 $list_constr_files }
+## TODO: consider how to determin connected port names...
+
+set pmod [file tail [file dirname [info script]]]
+for {set idx 0} {[llength [get_bd_cells /${pmod}_${idx}]] > 0} {incr idx} {}
+set nameHier ${pmod}_${idx}; # Find first unused trailing number for a name like PmodNAV_0
+
+# call the process created in the bd.tcl script. recreates the described hierarchy
+source [file join [file dirname [info script]] "bd.tcl"]
+create_hier_cell_${pmod}_* / $nameHier

124ff79bc9dc065647eba1fcfd1d0ca5744e5225
ArtVVB <arthur.brown@digilent.com>
2019-07-31 11:43:35 -0700

    resolve potential naming conflict issue with XDC files

--- PmodAD1/create_hier.tcl
+++ PmodAD1/create_hier.tcl
@@ -33,17 +33,33 @@ set pmod [file tail [file dirname [info script]]]
 for {set idx 0} {[llength [get_bd_cells /${pmod}_${idx}]] > 0} {incr idx} {}
 set nameHier ${pmod}_${idx}; # Find first unused trailing number for a name like PmodNAV_0
 
-# Create temporary copies of each constraint file, affix the hierarchy name to them, then import them into the project
+# Create temporary copies of each constraint file,
+#  add the hierarchy name to the name as an affix, 
+#  if a file with the same name already exists, add a number as a suffix
+#  then import them into the project
 set list_constr_files [glob -nocomplain [file join [file dirname [info script]] constrs *]]
 set tempdir [file join [file dirname [info script]] temp]
 file mkdir $tempdir
+set existing_constrs []
+foreach constr [get_files -of_objects [get_filesets constrs_1]] {
+	lappend existing_constrs [file tail ${constr}]
+}
 foreach constr $list_constr_files {
-	file copy ${constr} [file join ${tempdir} ${nameHier}_[file tail ${constr}]]
+	set constr_tail [file rootname [file tail ${constr}]]
+	set constr_ext [file extension ${constr}]
+	set filename ${nameHier}_${constr_tail}${constr_ext}
+	if {[lsearch $existing_constrs ${filename}] == -1} {
+		file copy ${constr} [file join ${tempdir} ${filename}]
+	} else {
+		for {set idx 0} {[lsearch $existing_constrs ${nameHier}_${constr_tail}_${idx}${constr_ext}] != -1} {incr idx} {}
+		set filename ${nameHier}_${constr_tail}_${idx}${constr_ext}
+		file copy ${constr} [file join ${tempdir} ${filename}]
+	}
 }
 set list_temp_constr_files [glob -nocomplain [file join ${tempdir} *]]
 catch { import_files -fileset constrs_1 $list_temp_constr_files }
 file delete -force $tempdir
 
 # call the process created in the bd.tcl script. recreates the described hierarchy
-source [file join [file dirname [info script]] "bd.tcl"]
+source -notrace [file join [file dirname [info script]] "bd.tcl"]
 create_hier_cell_${pmod}_* / $nameHier
